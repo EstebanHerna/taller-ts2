168ee9aef32979cba90de244909f4ada8374143c
Esteban Alejandro Hernandez Sulvara <142023857+EstebanHerna@users.noreply.github.com>
2024-10-19 21:02:14 -0500

    Add files via upload

---
+++ data.ts
@@ -0,0 +1,57 @@
+// data.js
+export const series = [
+    {
+        id: 1,
+        title: "Breaking Bad",
+        channel: "AMC",
+        seasons: 5,
+        description: "Set and filmed in Albuquerque, New Mexico, the series tells the story of Walter White, a struggling and depressed high school chemistry teacher who is diagnosed with lung cancer.",
+        link: "https://www.amc.com/shows/breaking-bad",
+        image: "https://i.imgur.com/GGje0vc.jpg"
+    },
+    {
+        id: 2,
+        title: "Orange Is the New Black",
+        channel: "Netflix",
+        seasons: 6,
+        description: "The series begins revolving around Piper Chapman, a woman in her thirties living in New York City who is sentenced to 15 months in Litchfield Penitentiary.",
+        link: "https://www.netflix.com/co/title/70242311",
+        image: "https://i.imgur.com/EvKe48G.jpg"
+    },
+    {
+        id: 3,
+        title: "Game of Thrones",
+        channel: "HBO",
+        seasons: 7,
+        description: "American fantasy drama based on the A Song of Ice and Fire novels by George R. R. Martin.",
+        link: "https://www.hbo.com/game-of-thrones",
+        image: "https://i.imgur.com/TDCEV1S.jpg"
+    },
+    {
+        id: 4,
+        title: "The Big Bang Theory",
+        channel: "CBS",
+        seasons: 12,
+        description: "Leonard and Sheldon are brilliant physicists, socially challenged everywhere except the lab. Penny, their neighbor, tries to teach them about life.",
+        link: "https://www.cbs.com/shows/big_bang_theory/about/",
+        image: "https://i.imgur.com/uAEpVWk.jpg"
+    },
+    {
+        id: 5,
+        title: "Sherlock",
+        channel: "BBC",
+        seasons: 4,
+        description: "Modern-day depiction of Sherlock Holmes solving mysteries in London with Dr. John Watson.",
+        link: "https://www.bbc.co.uk/programmes/b018ttws",
+        image: "https://i.imgur.com/02B7qhj.jpg"
+    },
+    {
+        id: 6,
+        title: "A Very English Scandal",
+        channel: "BBC",
+        seasons: 2,
+        description: "Fact-based three-part British comedy-drama about a political scandal.",
+        link: "https://www.bbc.co.uk/programmes/p065smy4",
+        image: "https://i.imgur.com/D4y3DrQ.jpg"
+    }
+];
